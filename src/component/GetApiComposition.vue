362107e73ceb8edc8101650c3972642c2a854b4d
vpv <vincentpaulvictor@gmail.com>
2022-07-01 16:37:14 +0200

    pagination

--- src/component/GetApiComposition.vue
+++ src/component/GetApiComposition.vue
@@ -2,19 +2,23 @@
   <p>Test de récupération de données avec AXIOS</p>
   <p>Nombre total de résultat: {{items.count}}</p>
   <h3>Listes des pokemons</h3>
-  
-  <p>A faire : un boutton pour aller sur la page suivante</p>
+  <ul>
+    <li v-for="item in items.results" :key="item"> {{item.name}}</li>
+  </ul>
+  <button @click="previous()">Précédent</button>
+  <button @click="next()">Suivant</button>
 </template>
 
 <script setup>
 import axios from "axios";
 import { ref } from 'vue'
 
-const items = ref([0]);
+const items = ref([]);
 const BASE_URL = "https://pokeapi.co/api/v2/pokemon/";
 
-const getPokemon = () => {
-  axios.get(BASE_URL)
+
+const getPokemon = (url) => {
+  axios.get(url)
     .then(function (response) {
       items.value = response.data;
     })
@@ -23,7 +27,18 @@ const getPokemon = () => {
     });
 }
 
-getPokemon()
+getPokemon(BASE_URL)
+
+const next = () => {
+  const next_url = items.value.next;
+  getPokemon(next_url)
+}
+
+const previous = () => {
+  const previous_url = items.value.previous;
+  getPokemon(previous_url)
+}
+
 
 </script>
 
